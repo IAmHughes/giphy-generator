f38eedcd9a9a8dbbda2498bc69b20f853d4a9787
Thomas Hughes <iamhughes@github.com>
2020-03-09 16:31:40 -0500

    Parse issue/pr number and body of message

--- dist/index.js
+++ dist/index.js
@@ -13991,10 +13991,29 @@ async function run() {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
     const github = new GitHub(process.env.GITHUB_TOKEN);
 
-    core.debug(`context: ${JSON.stringify(context)}`);
-
     // Get owner, repo, and event from context of payload that triggered the action
     const {owner, repo} = context.repo;
+    const event_type = context.eventName;
+    let issue_pr_number;
+    let body;
+
+    // Parse body of issue or PR to look for `/giphy <search_term>`
+    if (event_type === 'pull_request') {
+      issue_pr_number = context.number;
+      body = context.number.body;
+
+      core.debug(`Pull request triggered action, issue_pr_number: ${issue_pr_number}, body: ${body}`)
+    } else if (event_type === 'issues') {
+      issue_pr_number = context.issue.number;
+      body = context.issue.body;
+
+      core.debug(`Issue triggered action, issue_pr_number: ${issue_pr_number}, body: ${body}`)
+    } else {
+      issue_pr_number = context.issue.number;
+      body = context.comment.body;
+
+      core.debug(`Issue comment triggered action, issue_pr_number: ${issue_pr_number}, body: ${body}`)
+    }
 
     // Get the inputs from the workflow file: https://github.com/actions/toolkit/tree/master/packages/core#inputsoutputs
     const rating = core.getInput('rating', {required: false});
